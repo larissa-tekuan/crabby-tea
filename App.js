f01c4bf7a71ccf283f3670c73779a0d1829164d8
Larissa Bianca <larissa_bianca@outlook.com>
2020-08-15 13:13:00 -0300

    To do list app test

--- App.js
+++ App.js
@@ -1,38 +1,103 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import * as React from "react";
+import {
+  StatusBar,
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+export default class App extends React.Component {
+  state = {
+    inputValue: "",
+    todos: [],
+  };
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+  changeText = (value) => {
+    this.setState({
+      inputValue: value,
+    });
+  };
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </View>
-  );
+  addItem = () => {
+    if (this.state.inputValue !== "") {
+      this.setState((prevState) => {
+        const newToDo = {
+          title: this.state.inputValue,
+          createdAt: Date.now(),
+        };
+
+        var todos = this.state.todos.concat(newToDo);
+
+        this.setState({
+          todos: todos,
+        });
+      });
+    }
+  };
+
+  render() {
+    const todos = this.state.todos.reverse().map((todo, key) => (
+      <View style={{ flexDirection: "row", marginTop: 20 }}>
+        <TouchableOpacity
+          style={{
+            width: 20,
+            height: 20,
+            borderRadius: 15,
+            borderWidth: 3,
+            borderColor: "white",
+            margin: 15,
+          }}
+        ></TouchableOpacity>
+        <Text
+          style={{
+            paddingLeft: 5,
+            marginTop: 10,
+            fontSize: 28,
+            color: "white",
+          }}
+        >
+          {todo.title}
+        </Text>
+      </View>
+    ));
+
+    return (
+      <LinearGradient colors={["#667eea", "#764ba2"]} style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <View>
+          <TextInput
+            style={styles.input}
+            onSubmitEditing={this.addItem}
+            onChangeText={this.changeText}
+            placeholder="Type here to add a to do."
+            value={this.state.inputValue}
+            placeholderTextColor={"#fff"}
+            multiline={true}
+            autoCapitalize="sentences"
+            underlineColorAndroid="transparent"
+            selectionColor={"white"}
+            maxLength={30}
+            returnKeyType="done"
+            autoCorrect={false}
+            blurOnSubmit={true}
+          />
+        </View>
+        <View>{todos}</View>
+      </LinearGradient>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+const styles = {
+  input: {
+    marginTop: 30,
+    paddingTop: 10,
+    paddingRight: 15,
+    paddingLeft: 15,
+    fontSize: 34,
+    color: "white",
+    fontWeight: "500",
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+};
