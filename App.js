8d7d8dd49ddb31a4fcc1c0841e202c0188d36333
Larissa Bianca <larissa_bianca@outlook.com>
2020-08-21 01:39:12 -0300

    Refact: Refatora o carregamento do APP

--- App.js
+++ App.js
@@ -1,103 +1,26 @@
-import * as React from "react";
-import {
-  StatusBar,
-  View,
-  TextInput,
-  Text,
-  TouchableOpacity,
-} from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+//Cuida da parte de gestos do native
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { AppLoading } from "expo";
+import { useFonts, Anton_400Regular } from "@expo-google-fonts/anton";
 
-export default class App extends React.Component {
-  state = {
-    inputValue: "",
-    todos: [],
-  };
+import Routes from "./router";
 
-  changeText = (value) => {
-    this.setState({
-      inputValue: value,
-    });
-  };
+export default function App() {
+  //Abre o app só depois de carregar a font
+  let [fontsLoaded] = useFonts({
+    Anton_400Regular,
+  });
 
-  addItem = () => {
-    if (this.state.inputValue !== "") {
-      this.setState((prevState) => {
-        const newToDo = {
-          title: this.state.inputValue,
-          createdAt: Date.now(),
-        };
-
-        var todos = this.state.todos.concat(newToDo);
-
-        this.setState({
-          todos: todos,
-        });
-      });
-    }
-  };
-
-  render() {
-    const todos = this.state.todos.reverse().map((todo, key) => (
-      <View style={{ flexDirection: "row", marginTop: 20 }}>
-        <TouchableOpacity
-          style={{
-            width: 20,
-            height: 20,
-            borderRadius: 15,
-            borderWidth: 3,
-            borderColor: "white",
-            margin: 15,
-          }}
-        ></TouchableOpacity>
-        <Text
-          style={{
-            paddingLeft: 5,
-            marginTop: 10,
-            fontSize: 28,
-            color: "white",
-          }}
-        >
-          {todo.title}
-        </Text>
-      </View>
-    ));
-
-    return (
-      <LinearGradient colors={["#667eea", "#764ba2"]} style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" />
-        <View>
-          <TextInput
-            style={styles.input}
-            onSubmitEditing={this.addItem}
-            onChangeText={this.changeText}
-            placeholder="Type here to add a to do."
-            value={this.state.inputValue}
-            placeholderTextColor={"#fff"}
-            multiline={true}
-            autoCapitalize="sentences"
-            underlineColorAndroid="transparent"
-            selectionColor={"white"}
-            maxLength={30}
-            returnKeyType="done"
-            autoCorrect={false}
-            blurOnSubmit={true}
-          />
-        </View>
-        <View>{todos}</View>
-      </LinearGradient>
-    );
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
-}
 
-const styles = {
-  input: {
-    marginTop: 30,
-    paddingTop: 10,
-    paddingRight: 15,
-    paddingLeft: 15,
-    fontSize: 34,
-    color: "white",
-    fontWeight: "500",
-  },
-};
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#000" translucent={true} />
+      <Routes />
+    </>
+  );
+}
